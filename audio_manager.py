3d58040ae9476e58058fb66f2012aef43083a044
nick good <nicksogood@hotmail.com>
2023-03-03 21:14:18 -0800

    feat: adds basic file playing

--- audio_manager.py
+++ audio_manager.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from typing import Iterable, Iterator
+import simpleaudio
 
 class AudioMemoDirectory(Iterable):
 	def __init__(self, memo_dir: Path):
@@ -16,6 +17,9 @@ class AudioMemo:
 
 	def play(self):
 		print(f'playing file {self.filepath}!')
+		wave_obj = simpleaudio.WaveObject.from_wave_file(str(self.filepath))
+		play_obj = wave_obj.play()
+		play_obj.wait_done()
 
 	def delete(self):
 		print(f'deleting file {self.filepath}')
